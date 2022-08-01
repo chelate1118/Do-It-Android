662ac2ed7822bc5002ec391a794ddc11a92d2022
Ryan <gs21077@gs.hs.kr>
2022-08-01 11:17:56 +0900

    table & frame layout

--- app/src/main/java/com/example/learn3/MainActivity.java
+++ app/src/main/java/com/example/learn3/MainActivity.java
@@ -3,12 +3,40 @@ package com.example.learn3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView imageView;
+    private ImageView imageView2;
+
+    int imageIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.gravity);
+        setContentView(R.layout.frame);
+
+        imageView = findViewById(R.id.imageView);
+        imageView2 = findViewById(R.id.imageView2);
+    }
+
+    public void onButton1Clicked(View v) {
+        changeImage();
+    }
+
+    private void changeImage() {
+        if (imageIndex == 0) {
+            imageView.setVisibility(View.VISIBLE);
+            imageView2.setVisibility(View.INVISIBLE);
+
+            imageIndex = 1;
+        } else {
+            imageView.setVisibility(View.INVISIBLE);
+            imageView2.setVisibility(View.VISIBLE);
+
+            imageIndex = 0;
+        }
     }
 }
