5c3880c020daaec9c72ce489703871873ee35b2d
Ryan <gs21077@gs.hs.kr>
2022-08-08 10:50:40 +0900

    button, text

--- app/src/main/java/com/example/learn4/MainActivity.java
+++ app/src/main/java/com/example/learn4/MainActivity.java
@@ -1,14 +1,23 @@
 package com.example.learn4;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText textBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.edit_text);
+
+        textBox = findViewById(R.id.editTextTextPersonName);
+    }
+
+    public void onSelected(View view) {
+        textBox.selectAll();
     }
 }
